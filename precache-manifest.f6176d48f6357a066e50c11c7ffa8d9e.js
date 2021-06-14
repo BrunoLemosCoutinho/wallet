c852debcd14250e833923fb20288697d3a9065ce
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-06-14 10:55:49 -0300

    Updates

---
+++ precache-manifest.f6176d48f6357a066e50c11c7ffa8d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea0d78996652ad6f67ab1041a73c4f7",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "6c6e0ba5d8da48b3261a",
+    "url": "/wallet/static/css/main.b7baae0a.chunk.css"
+  },
+  {
+    "revision": "f23f79064787ce351f97",
+    "url": "/wallet/static/js/2.391d58ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/wallet/static/js/2.391d58ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6e0ba5d8da48b3261a",
+    "url": "/wallet/static/js/main.560850f9.chunk.js"
+  },
+  {
+    "revision": "4fb4cea7ccc09b9511f8",
+    "url": "/wallet/static/js/runtime-main.8f1525cf.js"
+  }
+]);
